3b6c6401400a6ab804df0fc78b1bce5bd959835c
Reyhan Genç <reyhangenc.358@gmail.com>
2024-12-21 23:08:07 +0300

    düzenleme

--- BerberKuafor/Program.cs
+++ BerberKuafor/Program.cs
@@ -24,19 +24,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=AnaSayfa}/{action=Index}/{id?}");
-
+name: "default",
+pattern: "{controller=AnaSayfa}/{action=Index}/{id?}");
 app.MapRazorPages();
 
-//Run
 app.Run();
